377cf8f4a7849c798e2353073a826a390b60c5a7
Tinco Andringa <mail@tinco.nl>
2016-09-06 21:00:46 +0200

    build ruby 2.1.9 and 2.3.1

--- ruby/build.rb
+++ ruby/build.rb
@@ -1,3 +1,5 @@
+ROOT_DIR = File.expand_path(File.dirname(__FILE__))
+
 def main
 	versions = {
 		old: ['2.1.9'],
@@ -14,13 +16,15 @@ def main
 
 	puts "Generated keys."
 
-	puts "Building packages.."
-
 	all_versions.each do |version|
 		# Build packages
 		threads << Thread.new do
-			build_package(version)
-			# Build container with package added
+			puts "Going to build packages for #{version}"
+			build_packages(version)
+			puts "--------------------"
+			puts "Going to build container for #{version}"
+			build_container(version)
+			puts "--------------------"
 			# Tag container
 		end
 	end
@@ -30,40 +34,56 @@ def main
 end
 
 def generate_keys
-	root_dir = File.expand_path(File.dirname(__FILE__))
-	keys = "#{root_dir}/keys"
+	keys = "#{ROOT_DIR}/keys"
 
 	return unless `ls #{keys}`.empty?
 
 	puts "No keys present."
 
-	output = `docker run \
+	output = system %Q{ docker run \
 	  --rm -it \
 	  --entrypoint abuild-keygen \
 	  -v #{keys}:/home/builder/.abuild/ \
 	  -e PACKAGER="Tinco Andringa<tinco@phusion.nl>" \
-	  andyshinn/alpine-abuild:v2 -n`
+	  andyshinn/alpine-abuild:v2 -n \
+    }
 	raise "Generate keys failed:\n#{output}" unless $?.success?
 
 	puts "Generate keys output: #{output}"
-	puts `mv #{keys}/*.pub #{keys}/phusion.rsa.pub`
-	puts `mv #{keys}/*.rsa #{keys}/phusion.id_rsa`
+	system %Q{ mv #{keys}/*.pub #{keys}/phusion.rsa.pub }
+	system %Q{ mv #{keys}/*.rsa #{keys}/phusion.rsa.priv }
+	system %Q{ echo "Elevating priviliges to fix permissions of key files."; sudo chmod -R a+r #{keys} }
 end
 
-def build_package(name)
-	root_dir = File.expand_path(File.dirname(__FILE__))
-	package = "#{root_dir}/#{name}"
-	repository = "#{root_dir}/build-cache/packages"
-	keys = "#{root_dir}/keys"
+def build_container(version)
+	context = "#{ROOT_DIR}/context/#{version}"
+	repository = "#{ROOT_DIR}/build-cache/packages/#{version}"
+
+	system "mkdir -p #{context}"
+	system "cp -r #{repository} #{context}/packages"
+	system "cp #{ROOT_DIR}/context/shared/* #{context}/"
 	
-	output = `docker run -it \
-	    -e PACKAGER_PRIVKEY="/phusion.id_rsa" \
+	system "docker build --build-arg RUBY_VERSION=#{version} #{context}"
+end
+
+def build_packages(version)
+	package = "#{ROOT_DIR}/#{version}"
+	repository = "#{ROOT_DIR}/build-cache/packages/#{version}"
+
+	system %Q{ mkdir -p -m 777 #{repository}}
+
+	keys = "#{ROOT_DIR}/keys"
+	
+	output = system %Q{ docker run -it \
+		-e PACKAGER="Tinco Andringa<tinco@phusion.nl>" \
+	    -e PACKAGER_PRIVKEY="/phusion.rsa.priv" \
 	    -v "#{package}:/home/builder/package" \
 	    -v "#{repository}:/packages" \
 	    -v "#{keys}/phusion.rsa.pub:/etc/apk/keys/phusion.rsa.pub" \
-	    -v "#{keys}/phusion.id_rsa:/phusion.id_rsa" \
-	    andyshinn/alpine-abuild:v2`
-	raise "Build package #{name} failed:\n#{output}" unless $?.success?
+	    -v "#{keys}/phusion.rsa.priv:/phusion.rsa.priv" \
+	    phusion/abuild \
+	}
+	# raise "Build package #{version} failed:\n#{output}" unless $?.success?
 end
 
 main
